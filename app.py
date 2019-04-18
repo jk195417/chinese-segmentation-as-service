eac56c3cf0792f53fdc31aa3082bb09c140c46da
楊竑昕 <jk195417@gmail.com>
2019-04-18 18:47:05 +0800

    return array

--- app.py
+++ app.py
@@ -5,26 +5,40 @@ import pkuseg
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     return 'Chinese segmentation as service.'
 
+
 @app.route('/segmentations', methods=['POST'])
-def seg():
+def segmentations():
     text = request.json.get('text', False)
     if not text:
         return 'require text'
-
     global pku_seg
-    result = {
-        'jieba': ' '.join(jieba.cut(text)),
-        'snownlp': ' '.join(SnowNLP(text).words),
-        'pkuseg': ' '.join(pku_seg.cut(text))
-    }
+    if isinstance(text, list):
+        result = {
+            'jieba': [],
+            'snownlp': [],
+            'pkuseg': []
+        }
+        for sentence in text:
+            result['jieba'].append(jieba.lcut(sentence))
+            result['snownlp'].append(SnowNLP(sentence).words)
+            result['pkuseg'].append(pku_seg.cut(sentence))
+    else:
+        result = {
+            'jieba': jieba.lcut(text),
+            'snownlp': SnowNLP(text).words,
+            'pkuseg': pku_seg.cut(text)
+        }
     return jsonify(result)
 
+
 if __name__ == '__main__':
-    jieba.set_dictionary('dictionaries/big-dict.txt') # https://github.com/fxsjy/jieba/raw/master/extra_dict/dict.txt.big
+    # https://github.com/fxsjy/jieba/raw/master/extra_dict/dict.txt.big
+    jieba.set_dictionary('dictionaries/big-dict.txt')
     jieba.initialize()
     pku_seg = pkuseg.pkuseg()
     app.run('127.0.0.1', 3001)
